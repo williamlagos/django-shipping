fb4dcc8edb7680b252c0fd0d056e12f3ce129a89
William Oliveira de Lagos <william.lagos@live.com>
2014-03-13 00:59:57 -0300

    Fixed some problems in adding shipping tax to cartridge hook

--- shipping/hooks.py
+++ shipping/hooks.py
@@ -32,19 +32,15 @@ from shipping.fretefacil import FreteFacilShippingService
 from shipping.models import DeliverableProperty
 
 def fretefacil_shipping_handler(request, form, order=None):
-    #if request.session.get("free_shipping"): return
-    print 'Frete'
+    if request.session.get("free_shipping"): return
     settings.use_editable()
-    try: user_postcode = form.shipping_detail_postcode
-    except AttributeError,e: user_postcode = settings.CLIENT_POSTCODE
+    user_postcode = form.cleaned_data['shipping_detail_postcode']
     shippingservice = FreteFacilShippingService()
     cart = Cart.objects.from_request(request)
     delivery_value = 0.0
     if cart.has_items():
         for product in cart:
-            print product.sku
             properties = DeliverableProperty.objects.filter(sku=product.sku)
-            print properties
             if len(properties) > 0:
                 props = properties[0]
                 deliverable = shippingservice.create_deliverable(settings.STORE_POSTCODE,
