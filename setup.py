14c7632ac4d42d170b700f0f1f83343ec439e769
William Oliveira de Lagos <william.lagos@icloud.com>
2020-04-27 20:06:22 -0300

    Updated package version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ship',
-    version='0.2.6',
+    version='0.2.7',
     packages=['shipping'],
     include_package_data=True,
     license='LGPLv3 License',
